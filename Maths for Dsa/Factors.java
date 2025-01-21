2b182b8716f7663f17983f7f47943dac5cd7445b
Kasaksaxena <kasaksaxena6@gmail.com>
2025-01-21 20:16:55 +0530

    More optimized factors

--- Maths for Dsa/Factors.java
+++ Maths for Dsa/Factors.java
@@ -1,8 +1,12 @@
 
+import java.util.ArrayList;
+
+
 
 public class Factors{
     public static void main(String[] args) {
-        factors2(20);
+        factors3(20);
+
     }
     //time complexity O(n)
     static void factors1(int n){
@@ -26,4 +30,26 @@ public class Factors{
             }
         }
     }
+    
+// time and space complexity O(root n)
+    static void factors3(int n){
+        ArrayList<Integer> list=new ArrayList<>();//stores n/i values temporarely
+        for(int i=1;i<= Math.sqrt(n);i++){
+            if(n%i==0){
+               if(n/i==i){
+                 System.out.println(i);
+               } 
+               else{
+               System.out.println(i + " ");
+               list.add(n/i);
+               }
+            }
+}
+    for(int i=list.size()-1 ;i >=0;i--) {
+        System.out.print(list.get(i) + " ");
+
+    }
+
+
+}
 }
