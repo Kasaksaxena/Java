fd95a0f3a17b4047b7323f93de15aeb901e675b4
Kasaksaxena <kasaksaxena6@gmail.com>
2025-01-28 21:26:36 +0530

    return list without passing argument

--- Recursion/Arrays ques/Arraylist.java
+++ Recursion/Arrays ques/Arraylist.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 public class Arraylist{
     public static void main(String[] args){
-        int[] arr={2,3,1,4,5,4,9};
+        int[] arr={4,3,1,4,5,4,9};
         ArrayList<Integer> ans= findAllIndex(arr,4,0, new ArrayList<>());
-        System.out.println(ans);
+        System.out.println(findAllIndex2(arr,4,0));
 
     }
 
@@ -15,5 +15,22 @@ public class Arraylist{
         list.add(index);
       }
       return findAllIndex(arr, target, index+1, list);
+    }
+
+    static ArrayList<Integer> findAllIndex2(int[] arr,int target, int index){
+      
+      ArrayList<Integer> list= new ArrayList<>();
+      if(index == arr.length){
+        return list;
+      }
+      // this will contain ans for that function only
+      if(arr[index]== target){
+        list.add(index);
+      }
+
+      ArrayList<Integer> ansFromBelowCalls =findAllIndex2(arr, target, index+1);
+      list.addAll(ansFromBelowCalls);
+
+      return list;
     }
 }
