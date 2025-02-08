9d92d255e03ccbf20031a6d1059ef3e24fdfeece
Kasaksaxena <kasaksaxena6@gmail.com>
2025-02-08 18:06:33 +0530

    Subsequence with ASCII value

--- Recursion/Strings/Subseq.java
+++ Recursion/Strings/Subseq.java
@@ -1,7 +1,14 @@
 
+import java.util.ArrayList;
+
+
 public class Subseq{
     public static void main(String[] args) {
         subseq("","abc");
+       System.out.println( subseq1("","abc"));
+       System.out.println( subseqASCI("","abc"));
+
+
     }
 
     static void subseq( String p , String up){
@@ -14,4 +21,42 @@ public class Subseq{
         subseq(p,up.substring(1));
 
     }
+    static ArrayList<String> subseq1( String p , String up){
+        if(up.isEmpty()){
+          ArrayList<String> list=new ArrayList<>();
+          list.add(p);
+          return list;
+        
+        }
+        char ch=up.charAt(0);
+       ArrayList<String> left= subseq1(p+ch, up.substring(1));
+       ArrayList<String> right= subseq1(p, up.substring(1));
+
+       left.addAll(right);
+       return left;
+
+
+    }
+
+    static ArrayList<String> subseqASCI( String p , String up){
+        if(up.isEmpty()){
+          ArrayList<String> list=new ArrayList<>();
+          list.add(p);
+          return list;
+        
+        }
+        char ch=up.charAt(0);
+       ArrayList<String> first= subseqASCI(p+ch, up.substring(1));
+       ArrayList<String> second= subseqASCI(p, up.substring(1));
+       ArrayList<String> third= subseqASCI(p+(ch +0), up.substring(1));
+
+
+       first.addAll(second);
+       first.addAll(third);
+
+       return first ;
+
+
+    }
+
 }
