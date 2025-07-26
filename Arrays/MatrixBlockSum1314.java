1b0b8f24c4a5835c85e157da5fb8ca3194f7c276
Kasaksaxena <kasaksaxena6@gmail.com>
2025-07-26 21:54:00 +0530

    Leetcode 1314

---
+++ Arrays/MatrixBlockSum1314.java
@@ -0,0 +1,56 @@
+public class MatrixBlockSum1314 {
+    public static int[][] matrixBlockSum(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] prefix = new int[m + 1][n + 1];
+
+        // Step 1: Build prefix sum matrix
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefix[i][j] = mat[i - 1][j - 1] 
+                             + prefix[i - 1][j] 
+                             + prefix[i][j - 1] 
+                             - prefix[i - 1][j - 1];
+            }
+        }
+
+        // Step 2: Compute result using block sum
+        int[][] result = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Calculate boundaries of block
+                int r1 = Math.max(0, i - k), c1 = Math.max(0, j - k);
+                int r2 = Math.min(m - 1, i + k), c2 = Math.min(n - 1, j + k);
+
+                // Convert to prefix indices (+1)
+                result[i][j] = prefix[r2 + 1][c2 + 1] 
+                             - prefix[r1][c2 + 1] 
+                             - prefix[r2 + 1][c1] 
+                             + prefix[r1][c1];
+            }
+        }
+
+        return result;
+    }
+
+    // Sample test case
+    public static void main(String[] args) {
+        int[][] mat = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int k = 1;
+
+        int[][] ans = matrixBlockSum(mat, k);
+
+        // Print the result
+        for (int[] row : ans) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
