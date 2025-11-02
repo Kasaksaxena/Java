4d7c0f86eda766e167337cdb2b7997f140ab88cf
Kasaksaxena <kasaksaxena6@gmail.com>
2025-11-02 14:11:16 +0530

    T.p Leetcode 11

---
+++ Two pointers/Leetcode11.java
@@ -0,0 +1,33 @@
+
+
+ public class Leetcode11{
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            // calculate area
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+
+            maxArea = Math.max(maxArea, area);
+
+            // move the smaller height pointer
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println("Maximum Water Area: " + maxArea(height));
+    }
+} 
