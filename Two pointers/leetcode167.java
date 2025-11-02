16ee3b94f70f5b8d5f5ab30c5f1c94ff0d1515a2
Kasaksaxena <kasaksaxena6@gmail.com>
2025-11-02 14:04:33 +0530

    Two pointers (TP) LC 167

---
+++ Two pointers/leetcode167.java
@@ -0,0 +1,29 @@
+public class leetcode167 {
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1}; // 1-based index
+            } else if (sum < target) {
+                left++; // need a bigger sum
+            } else {
+                right--; // need a smaller sum
+            }
+        }
+
+        return new int[]{-1, -1}; // just in case, though problem guarantees one solution
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(numbers, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
