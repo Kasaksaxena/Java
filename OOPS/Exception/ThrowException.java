876d9524bdd31ce7884d9a7649e98b47b627dbbd
Kasaksaxena <kasaksaxena6@gmail.com>
2026-02-10 21:56:07 +0530

    Throw Exception

---
+++ OOPS/Exception/ThrowException.java
@@ -0,0 +1,18 @@
+
+public class ThrowException {
+    
+
+    static void checkAge(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("Not eligible to vote");
+        }
+        System.out.println("Eligible to vote");
+    }
+
+    public static void main(String[] args) {
+        checkAge(16);
+    }
+}
+
+    
+
