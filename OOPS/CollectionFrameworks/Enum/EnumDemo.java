5b7998fce0f4e9b0ccb588b87cb65907e8826411
Kasaksaxena <kasaksaxena6@gmail.com>
2026-02-11 21:13:35 +0530

    Enum

---
+++ OOPS/CollectionFrameworks/Enum/EnumDemo.java
@@ -0,0 +1,11 @@
+enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
+}
+public class EnumDemo {
+    public static void main(String[] args) {
+
+        Day today = Day.MONDAY;
+
+        System.out.println(today);
+    }
+}
