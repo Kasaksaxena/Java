3da0fdab31efccde53c07b1a0c4fff063fe688b1
Kasaksaxena <kasaksaxena6@gmail.com>
2026-02-24 20:44:23 +0530

    find middle in linked list

---
+++ Linkedlist/MiddleOfLinkedList.java
@@ -0,0 +1,50 @@
+public class MiddleOfLinkedList {
+    
+    static class ListNode{
+        int val;
+        ListNode next;
+    
+       ListNode(int val){
+          this.val = val;
+    }
+}
+
+public static void display(ListNode head){
+    while(head!= null ){
+        System.out.println(head.val+ " -> ");
+        head = head.next;
+    }
+}
+
+public  static ListNode middleLinkedlist(ListNode head){
+    ListNode slow = head;
+    ListNode fast = head;
+    while(fast !=null && fast.next!= null){
+        slow = slow.next;
+        fast= fast.next.next;
+
+    }
+    return slow;
+
+}
+public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        ListNode n6 = new ListNode(6);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+        ListNode middle = middleLinkedlist(n1);
+
+        System.out.println("Middle node: " + middle.val);
+
+
+}
+
+}
